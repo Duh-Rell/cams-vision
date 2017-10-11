f5c6a979cfe4a80a588912ece6be6937fe5eb502
duhre <duhre@DESKTOP-01G1TOE>
2017-10-11 17:57:11 -0400

    Add project files.

---
+++ CamsVision/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CamsVision.Startup))]
+namespace CamsVision
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
